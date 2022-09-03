c361508fd253def6e7c9699832d2dfa79c6d444c
[Dinma-Faith] <[phaytep@gmail.com]>
2022-09-03 23:44:27 +0100

    Add associations and validations to tables

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
-class Group < ApplicationRecord
-end
+class Category < ApplicationRecord
+    belongs_to :user
+    has_many :group_records, dependent: :destroy
+    has_many :records, through: :group_records, dependent: :destroy
+
+    validates :name, presence: true
+    validates :icon, presence: true
+  end
